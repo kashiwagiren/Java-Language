a92bd871fe287c038920ea6deaba4def8d46707d
Ren <130743896+KashiwagiRen@users.noreply.github.com>
2023-05-03 03:45:37 +0000

    qwerty

--- Java/Main.java
+++ Java/Main.java
@@ -1,7 +1,18 @@
-public class Main {  
-    public static void main(String[] args) {    
-    Second myCar = new Second();
-    myCar.fullThrottle();
-    myCar.speed(200);
-    }   
-} 
+public class Main {
+    // Static method
+    static void myStaticMethod() {
+      System.out.println("Static methods can be called without creating objects");
+    }
+    // Public method
+    public void myPublicMethod() {
+      System.out.println("Public methods must be called by creating objects");
+    }
+    // Main method
+    public static void main(String[ ] args) {
+        myStaticMethod(); // Call the static method
+        // myPublicMethod(); This would output an error
+
+        Main myObj = new Main(); // Create an object of Main
+        myObj.myPublicMethod(); // Call the public method
+  }
+}
