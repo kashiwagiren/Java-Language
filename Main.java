2c811cc121f60738a5f7686e15d917888aa8b064
Ren <130743896+KashiwagiRen@users.noreply.github.com>
2023-05-03 03:25:03 +0000

    May 03, 2023

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {  
+    public static void main(String[] args) {    
+    Second myCar = new Second();
+    myCar.fullThrottle();
+    myCar.speed(200);
+    }   
+} 
---
+++ Second.java
@@ -0,0 +1,6 @@
+public class Second {  
+    public void fullThrottle() {    
+    System.out.println("The car is going as fast as it can!");  
+    }  
+    public void speed(int maxSpeed) {    System.out.println("Max speed is: " + maxSpeed);  }
+    }    
